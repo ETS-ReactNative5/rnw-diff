e034c99b7507d43a7631cfc4c4360b0f573f6d70
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2021-05-31 15:46:56 +0000

    Release cpp/0.62.0-preview.4

---
+++ RnDiffApp/windows/rndiffapp/App.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::rndiffapp::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::rndiffapp::implementation
+
+
